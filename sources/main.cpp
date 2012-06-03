cde513503b2a4c285c89e5ae6e59ca2cef77b512
Szymon Piatek <spiatheck@gmail.com>
2012-06-03 17:17:05 +0200

    main file cleanup

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,5 +1,4 @@
 #include <qapplication.h>
-#include <qtranslator.h>
 #include <qlibraryinfo.h>
 #include "../headers/PlotWindow.h"
 
@@ -7,12 +6,6 @@ int main(int argc, char *argv[])
 {	
 	QApplication app(argc, argv);
 	app.setApplicationName("Porównywanie krzywych");
-     
-	QString translatorFileName = QLatin1String("qt_");
-     translatorFileName += QLocale::system().name();
-     QTranslator *translator = new QTranslator(&app);
-     if (translator->load(translatorFileName, QLibraryInfo::location(QLibraryInfo::TranslationsPath)))
-         app.installTranslator(translator);
 
 	PlotWindow window;
     window.resize(800,400);
